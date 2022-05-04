81919042e431e568833f124388f403e37094a070
amisharana002 <amisharana43@gmail.com>
2022-05-04 10:02:40 +0530

    commit

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,44 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Calculator.css";
+
 export default function Calculator() {
+  const [result, setResult] = useState("");
+
+  const handleclick = (e) => {
+    e.preventDefault();
+    console.log(e.target.name);
+    setResult(result.concat(e.target.name));
+  };
+
+  const c = () => {
+    setResult(result.slice(0, -1));
+  };
+
+  const Clear = () => {
+    setResult(" ");
+  };
+  let previousOpr;
+  const calculate = () => {
+    previousOpr = result;
+    document.querySelector(".field1").textContent = previousOpr;
+    setResult(eval(result).toString());
+  };
   return (
     <>
       <div className="Calculator-Base">
         <div className="Cal-display">
-          <div className="field1">120 x 10.5</div>
-          <div className="field2">1260</div>
+          <div className="field1" id="field1"></div>
+          <div className="field2" id="field2">
+            {result}
+          </div>
         </div>
         <div className="Cal-buttons">
           <div className="design"></div>
           <div className="Cal-keys">
             <div>
-              <button className="btns">C</button>
-              <button className="btns">+/-</button>
-              <button className="btns">%</button>
-              <button className="btns">/</button>
+              <button onClick={Clear} className="btns">
+                Clr
+              </button>
+              <button onClick={c} className="btns">
+                C
+              </button>
+
+              <button name="%" onClick={handleclick} className="btns">
+                %
+              </button>
+              <button name="/" onClick={handleclick} className="btns">
+                /
+              </button>
             </div>
             <div>
-              <button className="btns">7</button>
-              <button className="btns">8</button>
-              <button className="btns">9</button>
-              <button className="btns">X</button>
+              <button name="7" onClick={handleclick} className="btns">
+                7
+              </button>
+              <button name="8" onClick={handleclick} className="btns">
+                8
+              </button>
+              <button name="9" onClick={handleclick} className="btns">
+                9
+              </button>
+              <button name="*" onClick={handleclick} className="btns">
+                *
+              </button>
             </div>
             <div>
-              <button className="btns">4</button>
-              <button className="btns">5</button>
-              <button className="btns">6</button>
-              <button className="btns">-</button>
+              <button name="4" onClick={handleclick} className="btns">
+                4
+              </button>
+              <button name="5" onClick={handleclick} className="btns">
+                5
+              </button>
+              <button name="6" onClick={handleclick} className="btns">
+                6
+              </button>
+              <button name="-" onClick={handleclick} className="btns">
+                -
+              </button>
             </div>
             <div>
-              <button className="btns">1</button>
-              <button className="btns">2</button>
-              <button className="btns">3</button>
-              <button className="btns">+</button>
+              <button name="1" onClick={handleclick} className="btns">
+                1
+              </button>
+              <button name="2" onClick={handleclick} className="btns">
+                2
+              </button>
+              <button name="3" onClick={handleclick} className="btns">
+                3
+              </button>
+              <button name="+" onClick={handleclick} className="btns">
+                +
+              </button>
             </div>
             <div>
-              <button className="btns zero">0</button>
-              <button className="btns">.</button>
-              <button className="btns equal-btn">=</button>
+              <button name="0" onClick={handleclick} className="btns zero">
+                0
+              </button>
+              <button name="." onClick={handleclick} className="btns">
+                .
+              </button>
+              <button onClick={calculate} className="btns equal-btn">
+                =
+              </button>
             </div>
           </div>
         </div>
